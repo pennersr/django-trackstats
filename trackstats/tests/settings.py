47a7f22633b9759a356fbc7ac3bf2dc7a262abc9
Raymond Penners <raymond.penners@intenct.nl>
2023-02-15 16:22:41 +0100

    chore(settings): Tweak test settings.py

--- trackstats/tests/settings.py
+++ trackstats/tests/settings.py
@@ -1,22 +1,43 @@
-DATABASE_ENGINE = 'sqlite3'
+DATABASE_ENGINE = "sqlite3"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
     }
 }
 
-INSTALLED_APPS = [
-    'django.contrib.contenttypes',
-    'django.contrib.sites',
-    'django.contrib.auth',
-    'django.contrib.admin',
+INSTALLED_APPS = (
+    "django.contrib.contenttypes",
+    "django.contrib.sites",
+    "django.contrib.auth",
+    "django.contrib.admin",
+    "django.contrib.messages",
+    "trackstats",
+    "trackstats.tests",
+)
 
-    'trackstats',
-    'trackstats.tests'
+MIDDLEWARE = (
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+)
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": (
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ),
+        },
+    }
 ]
+
 
 USE_TZ = True
 
-SECRET_KEY = 'psst'
+SECRET_KEY = "psst"
