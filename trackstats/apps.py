dfd1677af5870108d3a9ca1a6facf0582fd36eaf
Emir Amanbekov <amanbekoff@gmail.com>
2023-07-24 19:26:14 +0600

    feat: Switch to BigAutoField
    * Set default auto field
    
    * Switch to BigAutoField
    
    * Add missing props to app config
    
    * Black-ify

---
+++ trackstats/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TrackStatsConfig(AppConfig):
+    name = "trackstats"
+    verbose_name = "TrackStats"
+    default_auto_field = "django.db.models.BigAutoField"
